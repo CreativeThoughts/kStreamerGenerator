d44a994102ea4d540f789dbc1a715e56ddeb8b92
CreativeThoughts <sunilmathew@sunils-mbp.home>
2017-07-23 15:35:51 +1000

    adding prompts to be async - returning promise

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,92 @@
 'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+var path = require('path');
+var util = require('util');
 
-module.exports = yeoman.generators.Base.extend({
-//Configurations will be loaded here.
-//Ask for user input
-prompting: function() {
-  var done = this.async();
-  this.prompt({
-    type: 'input',
-    name: 'name',
-    message: 'Your project name',
-    //Defaults to the project's folder name if the input is skipped
-    default: this.appname
-  }, function(answers) {
-    this.props = answers
-    this.log(answers.name);
-    done();
-  }.bind(this));
-},
-//Writing Logic here
+var _ = require('lodash');
+var Generator = require('yeoman-generator');
+var moment = require('moment');
 
-});
+module.exports = class extends Generator {
+
+  constructor(args, opts) {
+    super(args, opts);
+    //this is optional
+    this.argument('namespace', { type: String, required: false, optional: true, desc: 'namespace for the application'});
+  }
+
+  prompting () {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'appName',
+        message: 'Application Name',
+        default: path.basename(process.cwd())
+      },
+      {
+        type: 'input',
+        name: 'namespace',
+        message: 'Application Namespace: starts with au.com',
+        default: this.namespace
+      },
+      {
+        type: 'input',
+        name: 'processorName',
+        message: 'Stream Processor Name',
+        default: defaultEmail
+      }
+    ]).then((answers) => {
+      this.appName = _.camelCase(answers.appName);
+      this.processorName = _.kebabCase(answers.processorName);
+      this.namespace = answers.namespace.toLowerCase;
+      this.log('   Application: %s -> %s', this.namespace, this.appName);
+      this.log('   Processor Name:      %s', this.processorName);
+    });
+  }
+
+  paths () {
+    this.log('Confirm your project path: %s', this.destinationPath());
+    //will use it later
+    //var now = moment();
+
+    var model = {
+      appName: this.appName,
+      processorName: this.processorName,
+      namespace: this.namespace
+    };
+
+    
+    this.fs.copyTpl(
+      this._from('_gitignore'),
+      this._to('.gitignore'),
+      model
+    );
+    this.fs.copyTpl(
+      this._from('_readme.md'),
+      this._to('readme.md'),
+      model
+    );
+    this.fs.copyTpl(
+      this._from('_pom.xml'),
+      this._to('pom.xml'),
+      model
+    );   
+    // copy all java files
+    this.fs.copyTpl(
+      this._from('**/*.java'),
+      this._to(),
+      model
+    );    
+  }
+
+  //contextroot
+  _from(filename) {
+    return this.templatePath(filename);
+  }
+
+  _to(filename) {
+    if (filename) {
+      return this.destinationPath(filename);
+    }
+    return this.destinationRoot();
+  }  
+};
